518f8fd727e60efd1144c1a14be5af365f132f50
dilli1492001 <dilliprasad149@gmail.com>
2025-09-29 09:53:59 +0530

    password

--- pasword_validator.py
+++ pasword_validator.py
@@ -1,13 +1,12 @@
 while True:
     password = input("Create a password: ")
     
-    # Initialize validations
+    
     length_valid = len(password) >= 8
     has_upper = False
     has_lower = False
     has_digit = False
-    
-    # Check each character
+
     for char in password:
         if char.isupper():
             has_upper = True
@@ -15,3 +14,17 @@ while True:
             has_lower = True
         elif char.isdigit():
             has_digit = True
+
+    if length_valid and has_upper and has_lower and has_digit:
+        print("Password is valid!")
+        break
+    else:
+        print("Password must be at least 8 characters and contain:")
+        if not length_valid:
+            print("- At least 8 characters")
+        if not has_upper:
+            print("- At least one uppercase letter")
+        if not has_lower:
+            print("- At least one lowercase letter")
+        if not has_digit:
+            print("- At least one digit")
